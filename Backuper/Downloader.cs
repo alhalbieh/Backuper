aff8180a5f1e60f176d2b1ec6667ed7b5348441d
Ahmad <ahmadhalabieh16@gmail.com>
2022-05-27 15:57:54 +0300

    Added support for admin download

--- Backuper/Downloader.cs
+++ Backuper/Downloader.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -12,51 +13,68 @@ namespace Backuper
 {
     public class Downloader
     {
-        private IWebDriver driver;
+        private IWebDriver scrapper;
+        private IWebDriver opener;
         private Regex regex = new(@"\d+\.?\d+");
         private bool isAdmin;
 
-        public Downloader(IWebDriver driver, bool isAdmin)
+        public Downloader(IWebDriver driver, IWebDriver opener, bool isAdmin)
         {
-            this.driver = driver;
+            this.scrapper = driver;
+            this.opener = opener;
             this.isAdmin = isAdmin;
         }
 
-        public void CustomDownload(string mainName, string courseName, ReadOnlyCollection<IWebElement> subFolders, float maxSize = float.PositiveInfinity)
+        public void CustomDownload(string mainName, string courseName, ReadOnlyCollection<IWebElement> subFolders,
+                                   float maxSize = float.PositiveInfinity)
         {
-            foreach (IWebElement subFolder in subFolders)
+            foreach (var subFolder in subFolders)
             {
-                IWebElement subAnchor = subFolder.FindElement(By.XPath("span/a"));
-                DirectoryInfo path = Directory.CreateDirectory(Path.Combine(mainName, courseName, subAnchor.GetAttribute("innerHTML")));
-                driver.Navigate().GoToUrl(subAnchor.GetAttribute("href"));
+                var subAnchor = subFolder.FindElement(By.XPath("span/a"));
+                var path = Directory.CreateDirectory(Path.Combine(mainName, courseName, subAnchor.GetAttribute("innerHTML")));
+                scrapper.Navigate().GoToUrl(subAnchor.GetAttribute("href"));
+                opener.Navigate().GoToUrl(subAnchor.GetAttribute("href"));
                 if (isAdmin)
+                {
                     DoProcessAdmin(maxSize, path);
+                }
                 else
+                {
                     DoProcess(maxSize, path);
+                }
             }
         }
 
         public void CustomDownload(string mainName, string courseName, string courseUrl, float maxSize = float.PositiveInfinity)
         {
-            DirectoryInfo path = Directory.CreateDirectory(Path.Combine(mainName, courseName));
-            driver.Navigate().GoToUrl(courseUrl);
+            var path = Directory.CreateDirectory(Path.Combine(mainName, courseName));
+            scrapper.Navigate().GoToUrl(courseUrl);
+            opener.Navigate().GoToUrl(courseUrl);
             if (isAdmin)
+            {
                 DoProcessAdmin(maxSize, path);
+            }
             else
+            {
                 DoProcess(maxSize, path);
+            }
         }
 
         private void DoProcess(float maxSize, DirectoryInfo path)
         {
             while (true)
             {
-                var pdfItems = driver.FindElements(By.XPath("//ul[@class='fileListing']/li"));
+                var pdfItems = scrapper.FindElements(By.XPath("//ul[@class='fileListing']/li"));
                 DownloadList(pdfItems, path.FullName, maxSize);
-                var buttons = driver.FindElements(By.Id("nextLink"));
+                var buttons = scrapper.FindElements(By.Id("nextLink"));
                 if (buttons.Count > 0 && buttons[0].GetAttribute("class") != "button icon arrowright disable")
-                    new Actions(driver).MoveToElement(buttons[0]).Click().Perform();
+                {
+                    new Actions(scrapper).MoveToElement(buttons[0]).Click().Perform();
+                }
                 else
+                {
                     break;
+                }
             }
         }
 
@@ -64,7 +82,7 @@ namespace Backuper
         {
             List<Task> tasks = new();
             List<Tuple<string, string>> pathes = new();
-            foreach (var pdfItem in pdfItems)
+            foreach (IWebElement? pdfItem in pdfItems)
             {
                 string fileSizeText = pdfItem.FindElement(By.XPath("span[@class='filesize']")).Text;
                 float fileSize = float.Parse(regex.Matches(fileSizeText)[0].Value);
@@ -89,23 +107,66 @@ namespace Backuper
             }
             Task.WaitAll(tasks.ToArray());
         }
+
         private void DoProcessAdmin(float maxSize, DirectoryInfo path)
         {
             while (true)
             {
-                var rows = driver.FindElements(By.XPath("//tbody/tr"));
-                //DownloadTable(rows, path.FullName, maxSize);
-                var buttons = driver.FindElements(By.XPath("//span[@class='Next']"));
+                var rowsList = scrapper.FindElements(By.XPath("//tbody/tr")).ToList();
+                if (rowsList.Count > 0)
+                {
+                    rowsList.RemoveAt(0);
+                }
+
+                var rows = new ReadOnlyCollection<IWebElement>(rowsList);
+
+                DownloadTable(rows, path.FullName, maxSize);
+                scrapper.Navigate().Refresh();
+                var buttons = scrapper.FindElements(By.XPath("//span[@class='next']"));
                 if (buttons.Count > 0)
-                    new Actions(driver).MoveToElement(buttons[0]).Click().Perform();
+                {
+                    new Actions(scrapper).MoveToElement(buttons[0]).Click().Perform();
+                }
                 else
+                {
                     break;
+                }
             }
         }
 
-        private void DownloadTable(ReadOnlyCollection<IWebElement> pdfItems, string folder, float maxSize)
+        private void DownloadTable(ReadOnlyCollection<IWebElement> rows, string folder, float maxSize)
         {
+            var tasks = new List<Task>();
+            var pathes = new List<Tuple<string, string>>();
+            foreach (var row in rows)
+            {
+                var cells = row.FindElements(By.XPath("td"));
+                string name = cells[1].Text;
+                string fileSizeText = cells[2].Text;
+                string url = cells[^1].FindElement(By.XPath("a")).GetAttribute("href");
 
+                float fileSize = float.Parse(fileSizeText);
+                if (fileSize <= maxSize)
+                {
+                    opener.Navigate().GoToUrl(url);
+                    Console.WriteLine(opener.Url.ToString());
+                    /*
+                    string path = Path.Combine(folder, name);
+
+                    pathes.Add(new(url, name));
+                    int i = 1;
+                    while (Directory.Exists(path))
+                    {
+                        path = $"{path}_{i}";
+                        i++;
+                    }
+
+                    WebClient webClient = new();
+                    webClient.DownloadFile(url, path);
+                    */
+                }
+            }
+            //Task.WaitAll(tasks.ToArray());
         }
     }
 }
--- Backuper/MainWindow.xaml.cs
+++ Backuper/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Windows;
 namespace Backuper
@@ -9,35 +13,38 @@ namespace Backuper
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
         public MainWindow()
         {
             InitializeComponent();
-
             ChromeOptions chromeOptions = new();
-            chromeOptions.AddArgument("headless");
-            IWebDriver driver = new ChromeDriver(chromeOptions);
-            IWebDriver scraper = new ChromeDriver(chromeOptions);
+            //chromeOptions.AddArgument("headless");
+            using var home = new ChromeDriver(chromeOptions);
+            using var scrapper = new ChromeDriver(chromeOptions);
+            using var opener = new ChromeDriver(chromeOptions);
 
-            DirectoryInfo cwd = Directory.CreateDirectory(@"C:\TestingApp");
+            var cwd = Directory.CreateDirectory(@"C:\TestingApp");
             Directory.SetCurrentDirectory(cwd.FullName);
 
-            driver.Navigate().GoToUrl("https://bank.engzenon.com");
-            scraper.Navigate().GoToUrl("https://bank.engzenon.com/login");
+            Login(home);
+            Login(scrapper);
+            Login(opener);
 
-            scraper.FindElement(By.Id("UserUsername")).SendKeys("AhmadHalabieh");
-            Thread.Sleep(1000);
-            scraper.FindElement(By.Id("UserPassword")).SendKeys("5fcf01ebb");
-            Thread.Sleep(1000);
-            scraper.FindElement(By.XPath("//button[@class='btn-icon submit']")).Click();
-            var accountNav = scraper.FindElements(By.XPath("//ul[@id='user-account-nav']/li/a/span"));
+            var accountNav = home.FindElements(By.XPath("//ul[@id='user-account-nav']/li/a/span"));
             bool isLoggedin = accountNav[1].Text == "Logout";
 
-            Downloader downloader = new(scraper, isLoggedin);
+            var downloader = new Downloader(scrapper, opener, isLoggedin);
 
-            var mainFolders = driver.FindElements(By.XPath("//div[@class='content-box']/ul/li"));
-            foreach (IWebElement mainFolder in mainFolders)
+            var mainFolders = home.FindElements(By.XPath("//div[@class='content-box']/ul/li"));
+
+            foreach (var mainFolder in mainFolders)
             {
                 Console.WriteLine(mainFolder.Text);
                 var courseFolders = mainFolder.FindElements(By.XPath("ul/li"));
@@ -49,17 +56,28 @@ namespace Backuper
                     var subFolders = courseFolder.FindElements(By.XPath("ul/li"));
                     Console.WriteLine(courseName);
                     if (subFolders.Count > 0)
-                        downloader.CustomDownload(mainFolder.Text, courseName, subFolders, 10);
+                    {
+                        downloader.CustomDownload(mainFolder.Text, courseName, subFolders);
+                    }
                     else
-                        downloader.CustomDownload(mainFolder.Text, courseName, courseUrl, 10);
+                    {
+                        downloader.CustomDownload(mainFolder.Text, courseName, courseUrl);
+                    }
                 }
                 Console.WriteLine("***********************************");
             }
-            this.Close();
-            driver.Quit();
-            scraper.Quit();
+
+        }
 
 
+        public void Login(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl("https://bank.engzenon.com/login");
+            driver.FindElement(By.Id("UserUsername")).SendKeys("AhmadHalabieh");
+            Thread.Sleep(250);
+            driver.FindElement(By.Id("UserPassword")).SendKeys("5fcf01ebb");
+            Thread.Sleep(250);
+            driver.FindElement(By.XPath("//button[@class='btn-icon submit']")).Click();
         }
     }
 }
