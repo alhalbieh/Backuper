0f5a7ffb7df84ce0d2d173eabab06eef3d0c02ba
Ahmad <ahmadhalabieh16@gmail.com>
2022-07-24 07:57:21 +0300

    Added models

---
+++ DataDownload/Models/Folder.cs
@@ -0,0 +1,7 @@
+﻿namespace DataDownload.Models;
+
+public class Folder
+{
+    public string Path { get; set; } = "";
+    public string Url { get; set; } = "";
+}
---
+++ DataDownload/Models/Pdf.cs
@@ -0,0 +1,7 @@
+﻿namespace DataDownload.Models;
+
+public class Pdf
+{
+    public string Name { get; set; } = "";
+    public string Url { get; set; } = "";
+}
